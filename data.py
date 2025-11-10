df4e4939015c00cd98a6eb2a799f261146228171
rmg-qa <gorohovikroman123@yandex.com>
2025-11-11 00:44:24 +0700

    Реализовал dotenv для скрытия конфиденциальных данных

--- data.py
+++ data.py
@@ -1,9 +1,16 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+my_email = os.getenv('MY_EMAIL')
+name_sender = os.getenv('NAME_SENDER')
+name_recipient = os.getenv('NAME_RECIPIENT')
+
 URL = 'https://demowebshop.tricentis.com/'
-email = "romagorohovik@gmail.com"
-password = "qa_guru_test123"
-request_body = {"giftcard_2.RecipientName": "Роман",
-                "giftcard_2.RecipientEmail": email,
-                "giftcard_2.SenderName": "Roman Gorokhovik",
-                "giftcard_2.SenderEmail": email,
+
+request_body = {"giftcard_2.RecipientName": name_recipient,
+                "giftcard_2.RecipientEmail": my_email,
+                "giftcard_2.SenderName": name_sender,
+                "giftcard_2.SenderEmail": my_email,
                 "addtocart_2.EnteredQuantity": '1'
                 }
--- tests_api/conftest.py
+++ tests_api/conftest.py
@@ -3,6 +3,8 @@ import pytest
 from selene import browser
 import requests
 import data
+import os
+from dotenv import load_dotenv
 
 
 @allure.title('Настройки браузера. Разрешение 1920х1080')
@@ -17,8 +19,12 @@ def browser_settings():
 @allure.title('Авторизация и получение cookie')
 @pytest.fixture(scope='function')
 def get_cookie_auth(browser_settings):
+    load_dotenv()
+    my_email = os.getenv('MY_EMAIL')
+    my_password = os.getenv('MY_PASSWORD')
     request = requests.post(data.URL + 'login',
-                            json={"email": data.email, "password": data.password, "RememberMe": False},
+                            json={"email": my_email, "password": my_password,
+                                  "RememberMe": False},
                             allow_redirects=False)
     cookie = request.cookies.get("NOPCOMMERCE.AUTH")
     browser.open(data.URL)
