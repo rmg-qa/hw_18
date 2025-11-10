92bee34e99056038cc35e03082f26ce1318747a1
rmg-qa <gorohovikroman123@yandex.com>
2025-11-10 22:22:51 +0700

    Небольшие правки

--- classes/cart.py
+++ classes/cart.py
@@ -1,20 +1,21 @@
 import allure
 import requests
 from selene import browser
+
 import data
 
 
 class Cart:
 
-    @allure.title('Очистка корзины')
-    def del_cart(self):
-        browser.element(".//span[text()='Shopping cart']").click()
-        browser.element("[name='removefromcart']").click()
-        browser.element('[value="Update shopping cart"]').click()
-
-    @allure.title('Добавление товара в корзину')
+    @allure.step('Добавляем товары в корзину')
     def add_item_in_cart(self, payload, cookie):
         request = requests.post(data.URL + "addproducttocart/details/2/1",
                                 data=payload,
                                 cookies={"NOPCOMMERCE.AUTH": cookie})
         return request
+
+    @allure.step('Удаляем товары в корзине')
+    def del_cart(self):
+        browser.element(".//span[text()='Shopping cart']").click()
+        browser.element("[name='removefromcart']").click()
+        browser.element('[value="Update shopping cart"]').click()
